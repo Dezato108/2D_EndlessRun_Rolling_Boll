c23e41f892bb840c19109837f488ccbfcb8f0243
Dez <68178086+Dezato108@users.noreply.github.com>
2022-11-03 16:06:24 +0700

    Prefab the obstacle

--- Assets/Scripts/Camera Scripts/CameraFollow.cs
+++ Assets/Scripts/Camera Scripts/CameraFollow.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField] GameObject player;
+    [SerializeField] float cameraOffset = 10f;
     private void LateUpdate()
     {
-        Vector3 newPosition = new Vector3(0, transform.position.y, player.gameObject.transform.position.z - 10);
+        Vector3 newPosition = new Vector3(0, transform.position.y, player.gameObject.transform.position.z - cameraOffset);
         transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * 100);
     }
 }
