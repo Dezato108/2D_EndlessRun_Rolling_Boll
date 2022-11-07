ef7a7cd4ce60b0a754bfadd4db5b3d1cd0542c1c
Dez <hiepg2@gmail.com>
2022-11-07 16:25:53 +0700

    Instantiate random obstacles

---
+++ Assets/Scripts/Object Spawner Scripts/ObjectSpawner.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject[] trianglePrefabs;
+    private Vector3 spawnObstaclePosition;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float distanceToHorizon = Vector3.Distance(player.gameObject.transform.position, spawnObstaclePosition);
+        if (distanceToHorizon<120)
+        {
+            SpawnTriangles();
+        }
+    }
+
+    private void SpawnTriangles()
+    {
+        spawnObstaclePosition = new Vector3(0, 0, spawnObstaclePosition.z + 30);
+        Instantiate(trianglePrefabs[UnityEngine.Random.Range(0, trianglePrefabs.Length)], spawnObstaclePosition, Quaternion.identity);
+    }
+}
